921365890758bfdd01277b617e47344e04d108f1
Artvell3000 <86819826+Artvell3000@users.noreply.github.com>
2024-09-02 17:56:42 +0700

    Reader

--- bin/test_fpdart.dart
+++ bin/test_fpdart.dart
@@ -8,6 +8,35 @@ final URL_ERROR_404 = 'http://universities.hipolabs.com/sarch';
 // ignore: non_constant_identifier_names
 final URL_CORRECT = 'http://universities.hipolabs.com/search';
 
+class Dict{
+  final u404 = 'http://universities.hipolabs.com/sarch';
+  final u200 = 'http://universities.hipolabs.com/search';
+}
+
+ReaderTaskEither<Dict, String, List<String>> requestWithReader() => ReaderTaskEither((dict){
+  return TaskEither<String, List<String>>.tryCatch(() async {
+    final Response response = await dio.get(
+      dict.u404,
+        queryParameters: {
+          'country':'Canada'
+          }
+    );
+
+    final List<String> returnList = [];
+    final data = response.data as List<dynamic>;
+
+    for(int i=0;i<data.length;i++){
+          final univer = data[i] as Map<String,dynamic>;
+          returnList.add(univer['name']);
+    }
+
+    return returnList;
+  },
+  (e,s){
+    return '$e\n$s';
+  }).run();
+});
+
 TaskEither<String, List<String>> request(){
   return TaskEither.tryCatch(() async {
     final Response response = await dio.get(
@@ -33,10 +62,13 @@ TaskEither<String, List<String>> request(){
 }
 
 void main() async{
-  final fetch = await request().run();
+  final fetch = requestWithReader();
 
-  print(fetch.getOrElse((str){
-    print(str);
+  final task = fetch.getOrElse((v){
     return [];
-  }));
+  });
+
+  final result = await task.run(Dict());
+
+  print(result);
 }
